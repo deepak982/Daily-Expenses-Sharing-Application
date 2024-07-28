e6ed4c57ac7a598712bbc67cad6aafd246818e24
Deepak Malik <89151706+deepak982@users.noreply.github.com>
2024-07-28 13:19:47 +0530

    add static dir

--- expense_sharing/settings.py
+++ expense_sharing/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'expenses',
     'rest_framework',
-    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
